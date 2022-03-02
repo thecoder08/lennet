5afe5dbb626ee7d92443cc3d867fdc246e534954
Lennon McLean <lmmclean08@gmail.com>
2022-03-01 20:08:12 -0500

    fixed a thing

--- lennet.js
+++ lennet.js
@@ -5,7 +5,7 @@ var http = require('@thecoder08/http');
 http.server(25568, function (req, res) {
   if (req.pathname == '/getlennetlobbyid') {
     var lobbyid = nextId.toString().padStart(3, '0');
-    cp.execSync(command.replace('%PORT%', '65' + lobbyid), {stdio: 'inherit'});
+    cp.exec(command.replace('%PORT%', '65' + lobbyid), {stdio: 'inherit'}, function(err, stderr, stdout) {});
     res(200, 'text/plain', lobbyid);
     nextId++;
   }
