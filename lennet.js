f3372e3b7032cd5fd1a656bbc1b5e18efab17525
Lennon McLean <lmmclean08@gmail.com>
2022-03-01 19:50:19 -0500

    redid it

--- lennet.js
+++ lennet.js
@@ -1,32 +1,16 @@
-var requestHandlers = {};
-var serverInstance = function() {};
-module.exports = {
-  registerRequestHandler: function(id, handler) {
-    requestHandlers[id] = handler;
-  },
-  setServerInstance: function(instance) {
-    serverInstance = instance;
-  },
-  init: function(port) {
-    var http = require('@thecoder08/http');
-    http.server(port, function(req, res, redirect) {
-      if (req.pathname == '/getlennetlobbyid') {
-        var letter1 = String.fromCharCode(Math.round(Math.random() * 26) + 64);
-        var letter2 = String.fromCharCode(Math.round(Math.random() * 26) + 64);
-        var letter3 = String.fromCharCode(Math.round(Math.random() * 26) + 64);
-        var letter4 = String.fromCharCode(Math.round(Math.random() * 26) + 64);
-        var id = letter1.concat(letter2, letter3, letter4);
-        serverInstance(id);
-        res(200, 'text/plain', id);
-      }
-      else {
-        if (requestHandlers.hasOwnProperty(req.query.lennetlobbyid)) {
-          requestHandlers[req.query.lennetlobbyid](req, res);
-        }
-        else {
-          res(404, 'text/plain', '404 lobby ' + req.query.lennetlobbyid + ' not found')
-        }
-      }
-    });
+module.exports.init = function(port, command) {
+var nextId = 0;
+var cp = require('child_process');
+var http = require('@thecoder08/http');
+http.server(25568, function (req, res) {
+  if (req.pathname == '/getlennetlobbyid') {
+    var lobbyid = nextId.toString().padStart(3, '0');
+    cp.execSync(command.replace('%PORT%', '65' + lobbyid), {stdio: 'inherit'});
+    res(200, 'text/plain', lobbyid);
+    nextId++;
   }
+  else {
+    res(404, 'text/plain', '404 not found');
+  }
+});
 }
