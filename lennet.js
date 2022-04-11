993e0364bdfb74b12a835180dec282d661b38ddd
Lennon McLean <lmmclean08@gmail.com>
2022-04-11 10:43:44 -0400

    Update lennet.js

--- lennet.js
+++ lennet.js
@@ -2,7 +2,7 @@ module.exports.init = function(port, command) {
 var nextId = 0;
 var cp = require('child_process');
 var http = require('@thecoder08/http');
-http.server(25568, function (req, res) {
+http.server(port, function (req, res) {
   if (req.pathname == '/getlennetlobbyid') {
     var lobbyid = nextId.toString().padStart(3, '0');
     cp.exec(command.replaceAll('%PORT%', '65' + lobbyid), {stdio: 'inherit'}, function(err, stderr, stdout) {});
